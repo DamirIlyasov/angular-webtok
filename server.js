325a8e22ec702829c6aa2a5d114a8476f03a4010
DamirIlyasov <damzik1997>
2018-08-23 01:06:08 +0300

    heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/your_app_name'));
+app.use(express.static(__dirname + '/dist/Angular-Basic-Video-Chat'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname + '/dist/your_app_name/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/Angular-Basic-Video-Chat/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
