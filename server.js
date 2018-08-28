64c8a20ea2a8827406e845cb445fa9d6813a5cc0
DamirIlyasov <damzik1997>
2018-08-29 00:52:25 +0300

    port changed to 80

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.get('/*', function (req, res) {
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 80);
 console.log('Server started');
